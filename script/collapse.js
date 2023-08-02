df7fa246077454acbd61d1b7e3ac1937944f7ad5
FranzOst <franz.ostrizek@gmail.com>
2023-08-02 19:59:51 +0200

    Refactor

--- script/collapse.js
+++ script/collapse.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
             this.classList.toggle("active");
+            // var content = this.nextElementSibling;
             var content = this.parentElement.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "flex";
-                this.innerHTML = '<i class="fa fa-fw fa-angle-down"></i>Show abstract';
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+				this.innerHTML = '<i class="fa fa-fw fa-angle-down"></i>Show abstract';
             } else {
-                content.style.display = "block";
-                this.innerHTML = '<i class="fa fa-fw fa-angle-up"></i>Hide abstract';
+                content.style.maxHeight = content.scrollHeight + "px";
+				this.innerHTML = '<i class="fa fa-fw fa-angle-up"></i>Hide abstract';
             }
         });
     }
