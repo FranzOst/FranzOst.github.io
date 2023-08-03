4fa06c28aeda08277ef7fb6ba8dd9d1e330d6c27
FranzOst <franz.ostrizek@gmail.com>
2023-08-03 20:15:31 +0200

    Revert "Removing jquery dependence"
    This reverts commit 4c25bfc2b06bca7e42b8e6b0620686dfa8e25aeb.

--- script/collapse.js
+++ script/collapse.js
@@ -1,10 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
     var coll = document.getElementsByClassName("collapsible");
     var i;
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
             this.classList.toggle("active");
+            // var content = this.nextElementSibling;
             var content = this.parentElement.nextElementSibling;
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
