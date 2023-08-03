4c25bfc2b06bca7e42b8e6b0620686dfa8e25aeb
FranzOst <franz.ostrizek@gmail.com>
2023-08-03 20:12:24 +0200

    Removing jquery dependence

--- script/collapse.js
+++ script/collapse.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
     var coll = document.getElementsByClassName("collapsible");
     var i;
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
             this.classList.toggle("active");
-            // var content = this.nextElementSibling;
             var content = this.parentElement.nextElementSibling;
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
